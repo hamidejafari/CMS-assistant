3ae6d9cc143573ef2b7337d1b8d962f185bf8316
sadjadgit@gmail.com <sadjadgit@gmail.com>
2024-04-05 15:46:13 +0330

    تغییر کانفیگ تغییر صفحه  جزیات خدمات سرویس  و خدمات

--- src/Modules/V1/Service.php
+++ src/Modules/V1/Service.php
@@ -1,8 +1,18 @@
 <?php
 
 namespace Rahweb\CmsAssistant\Modules\V1;
+use Illuminate\Support\Collection;
+use Rahweb\CmsAssistant\Core\API;
 
 class Service
 {
+    public static function services(): Collection
+    {
+        return API::get('v1/services');
+    }
 
+    public static function get(int $slug): Collection
+    {
+        return API::get('v1/service/' . $slug);
+    }
 }
