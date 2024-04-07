db232472a9ba6f0f3a2290cfc5193601c92bd969
Hamide <hamideh9696@gmail.com>
2024-04-07 09:33:38 +0330

    fix bug get ip

--- helpers.php
+++ helpers.php
@@ -3,11 +3,15 @@
 if (!function_exists('get_ip')) {
     function get_ip()
     {
-        $ipAddress = $_SERVER['REMOTE_ADDR'];
-        if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER)) {
-            $tmp = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
-            $ipAddress = array_pop($tmp);
+        if(isset($_SERVER['REMOTE_ADDR'])){
+            $ipAddress = $_SERVER['REMOTE_ADDR'];
+            if (array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER)) {
+                $tmp = explode(',', $_SERVER['HTTP_X_FORWARDED_FOR']);
+                $ipAddress = array_pop($tmp);
+            }
+            return trim($ipAddress);
+        }else{
+            return null;
         }
-        return trim($ipAddress);
     }
 }
