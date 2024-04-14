21f39db216ed7a72bd35227fee5312c29af3203f
Rahweb <rahwebprogrammer2@gmail.com>
2024-04-14 14:47:55 +0330

    فرم تماس باما dto contact سرویس contact

---
+++ src/Modules/V1/Contact.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Rahweb\CmsAssistant\Modules\V1;
+
+use Illuminate\Support\Collection;
+use Rahweb\CmsAssistant\Core\API;
+
+class Contact
+{
+    public static function create($request): Collection
+    {
+        return API::get('v1/create-contact');
+    }
+
+
+}
