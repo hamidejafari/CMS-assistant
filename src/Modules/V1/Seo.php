494ea37f6172e2b00c0a2a13baee8461f5d64583
Rahweb <rahwebprogrammer2@gmail.com>
2024-06-08 11:07:22 +0330

    seo

---
+++ src/Modules/V1/Seo.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Rahweb\CmsAssistant\Modules\V1;
+use Illuminate\Support\Collection;
+use Rahweb\CmsAssistant\Core\API;
+
+class Seo
+{
+
+    public static function getStatic($url): Collection
+    {
+        return API::get('v1/seo/' . $url);
+    }
+}
