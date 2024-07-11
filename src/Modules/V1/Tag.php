733f5acb38a661821811d4c8cfe43e6289de1fce
Rahweb <rahwebprogrammer2@gmail.com>
2024-07-11 09:01:43 +0330

    ای پی آی تگ

---
+++ src/Modules/V1/Tag.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Rahweb\CmsAssistant\Modules\V1;
+use Illuminate\Support\Collection;
+use Rahweb\CmsAssistant\Core\API;
+
+class Portfolio
+{
+    public static function getList(): Collection
+    {
+        return API::get('v1/tags');
+    }
+
+    public static function getDetail($url): Collection
+    {
+        return API::get('v1/tag/' . $url);
+    }
+}
